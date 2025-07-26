bb86b63d8a9424bb35e39a04122a8fa5704abab2
upupc <liuyuan516@gmail.com>
2025-07-26 14:04:49 +0800

    fix:移除 API 设置中的硬编码密钥
    - 删除了 use-settings.ts 文件中 defaultApiSettings 对象的 openrouterApiKey硬编码值
    - 将 openrouterApiKey 的默认值改为空字符串，提高安全性

--- hooks/use-settings.ts
+++ hooks/use-settings.ts
@@ -27,7 +27,7 @@ export interface APISettings {
 }
 
 const defaultApiSettings:APISettings = {
-  openrouterApiKey: 'sk-cbb74ca319bf421aa369b5b3f0089c08',
+  openrouterApiKey: '',
   openrouterModel: 'qwen-turbo-latest',
   provider:'qwen',
   baseUrl: 'https://dashscope.aliyuncs.com/compatible-mode/v1',
